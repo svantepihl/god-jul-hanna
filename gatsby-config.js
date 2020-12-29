548ac867a96c06b30266e0ecbfacba4015193ff3
Svante Pihl <hej@svantepihl.com>
2020-12-29 22:08:46 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Svante Pihl 2020`,
+    description: `God jul!`,
+    author: `Svante Pihl`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+
+import tw from "twin.macro"
+import React from "react"
+
+import "@fontsource/mountains-of-christmas"
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const Wrapper = tw.div`
+  flex items-center justify-center flex-col h-screen
+`
+
+const Main = tw.div`
+  p-6 bg-gray-100 rounded-lg shadow-2xl lg:w-1/2 w-full
+`
+
+const Heading = tw.h1`
+  text-2xl text-black
+`
+
+const Text = tw.p`
+  text-xl text-black
+`
+
+
+export default function Index() {
+  return (
+    <Layout>
+      <Wrapper>
+        <SEO title="God jul!" />
+        <Main>
+          <Heading>GOD JUL HANNA<span role="img">&#127876;</span> <br/>Önskar Svante<br/></Heading>
+          <Text className="pt-12 text-left">
+          <br/>Ära vare gud i höjden
+          <br/>denna har min syster gjort i slöjden<br/>
+
+          
+          </Text>
+
+        </Main>
+      </Wrapper>
+    </Layout>
+  )
+}
